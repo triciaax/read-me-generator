805ce3623eaa1a3e160fdeded4737e5d1b374c12
Tricia <tricianghiem@gmail.com>
2022-07-02 18:35:55 -0500

    Added functions to build table of contents

--- src/readme-template.js
+++ src/readme-template.js
@@ -1,3 +1,24 @@
+const addStringIfExists = (item, string) => {
+    if (item) {
+        return string
+    } else {
+        return null
+    }
+}
+// optional sections are contributors, test, and license - if they dont exist we dont add them to toc
+const buildToc = (contributors, test, license) => {
+    let tocString = `
+    ## Table of Contents
+    [Installation](#install)
+    [Usage](#usage)
+    ${addStringIfExists(contributors, "[Contribution Guidelines](#contrib)")}
+    ${addStringIfExists(test, "[Test Instructions](#test)")}
+    ${addStringIfExists(license, "[License Terms](#license)")}
+    [Questions](#questions)
+    `
+}
+
+
 const buildHeader = (title, description) => {
     return `
     # ${title}
@@ -5,7 +26,7 @@ const buildHeader = (title, description) => {
     `
 }
 
-const buildBody = ()
+
 
 module.exports = (templateData) => {
     
