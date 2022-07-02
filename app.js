b51057496659451b26c874ba6bbdc4676d6b639d
Tricia <tricianghiem@gmail.com>
2022-07-02 17:56:45 -0500

    initialized npm and added gitignore

--- app.js
+++ app.js
@@ -2,14 +2,6 @@ var inquirer = require("inquirer");
 const fs = require("fs");
 const generatePage = require("./src/page-template");
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
-
 const promptUser = () => {
   return inquirer.prompt(
     {
